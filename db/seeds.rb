48fd753f1dbd6f1e308bc145432236b3bbbe6a2a
Mike Waddell <waddellmusic@gmail.com>
2017-12-20 15:34:30 +0000

    removed portfolio from seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,8 +12,5 @@ Refinery::Pages::Engine.load_seed
 # Added by Refinery CMS Blog engine
 Refinery::Blog::Engine.load_seed
 
-# Added by Refinery CMS Portfolio Engine
-Refinery::Portfolio::Engine.load_seed
-
 # Added by Refinery CMS Inquiries engine
 Refinery::Inquiries::Engine.load_seed
