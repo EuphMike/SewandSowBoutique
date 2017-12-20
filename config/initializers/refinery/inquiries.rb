2abd1938bc6c0df39c18dd8e97259d3661c4e67a
Mike Waddell <waddellmusic@gmail.com>
2017-12-20 15:25:57 +0000

    fixied inquiries

--- config/initializers/refinery/inquiries.rb
+++ config/initializers/refinery/inquiries.rb
@@ -1,24 +1,24 @@
 Refinery::Inquiries.configure do |config|
   # Configure whether to show privacy link
-  #config.show_contact_privacy_link = false
+  config.show_contact_privacy_link = false
 
   # Configure whether to show phone number field
-  #config.show_phone_number_field = false
+  config.show_phone_number_field = false
 
   # Configure whether to show form field placeholders
-  #config.show_placeholders = true
+  config.show_placeholders = true
 
   # Configure whether inquiries marked as spam should also send a notification mail
-  #config.send_notifications_for_inquiries_marked_as_spam = false
+  config.send_notifications_for_inquiries_marked_as_spam = false
 
   # Configure name in 'from' email address
-  # config.from_name = "no-reply"
+  config.from_name = "no-reply"
 
   # Configure the URLs to use for the inquiry functionality.
-  # config.post_path           = "/contact"
-  # config.page_path_new       = "/contact"
-  # config.page_path_thank_you = "/contact/thank_you"
+  config.post_path           = "/contact"
+  config.page_path_new       = "/contact"
+  config.page_path_thank_you = "/contact/thank_you"
 
   # Toggle the spam filter
-  # config.filter_spam = true
+  config.filter_spam = true
 end
